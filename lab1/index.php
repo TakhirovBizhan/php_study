aee27af8d300b6e5feb718cdad82c6bc03aa7322
TakhirovBizhan <bijantakhirov2004@mail.ru>
2024-02-04 17:39:53 +0300

    lab1

---
+++ lab1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Лаба 1</title>
+</head>
+<body>
+    <header style="height: 100px; border: solid black 1px;display:flex; gap: 300px;">
+        <img src="1234.png" alt="Политех">
+        <h1>Лабораторная работа №1</h1>
+    </header>
+    <main style="display:flex; justify-content:center; margin-top: 100px;">
+    <?php 
+       echo "Hello, php!";
+    ?>
+    </main>
+    <footer></footer>
+</body>
+</html>
